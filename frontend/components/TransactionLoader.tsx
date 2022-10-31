fc9675aa203132b947980a0ebdaf390352ea56bf
branko96 <branko-96@hotmail.com>
2022-10-31 20:28:14 -0300

    Fix typescript

--- frontend/components/TransactionLoader.tsx
+++ frontend/components/TransactionLoader.tsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/react'
 import { MoonLoader } from 'react-spinners'
 
 const style = {
@@ -6,17 +5,17 @@ const style = {
     title: `font-semibold text-xl mb-12`,
 }
 
-const cssOverride = css`
-  display: block;
-  margin: 0 auto;
-  border-color: white;
-`
+const cssOverride = {
+    display: "block",
+    margin: 0,
+    borderColor: "white"
+}
 
 const TransactionLoader = () => {
     return (
         <div className={style.wrapper}>
             <div className={style.title}>Transaction in progress...</div>
-            <MoonLoader color={'#fff'} loading={true} css={cssOverride} size={50} />
+            <MoonLoader color={'#fff'} loading={true} cssOverride={cssOverride} size={50} />
         </div>
     )
 }
